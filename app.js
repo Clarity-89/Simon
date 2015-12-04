7954798f6f9d5493ff67616d1404cb0e9624ade5
alex <homes89@ukr.net>
2015-12-04 20:21:52 +0200

    Add random choice function

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 /**
  * Created by alex on 12/2/15.
  */
+$(document).ready(function () {
 
+    //An array of all the class names for the sectors
+    var sectors = ['.green', '.yellow', '.red', '.blue'];
+
+    // Arrays for user's and computer choices
+    var userChoices = [], computerChoices = [];
+
+
+    //function to get a random element from an array
+    function randomChoice(arr) {
+        var i = Math.ceil(Math.random() * arr.length - 1);
+        return arr[i];
+    }
+
+    console.log(randomChoice(sectors));
+});
