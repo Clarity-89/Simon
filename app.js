73ae037cf866025497e61a7611e43fb64f7c790a
alex <homes89@ukr.net>
2015-12-04 22:04:51 +0200

    Add playChoice func

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
  */
 $(document).ready(function () {
 
+    //assign jquery objs to vars
+    var yellow = $('.yellow'), blue = $('.blue'), green = $('.green'), red = $('.red');
     //An array of all the class names for the sectors
-    var sectors = ['.green', '.yellow', '.red', '.blue'];
+    var sectors = [green, yellow, red, blue];
 
     // Arrays for user's and computer choices
     var userChoices = [], computerChoices = [];
-
 
     //function to get a random element from an array
     function randomChoice(arr) {
@@ -16,5 +17,15 @@ $(document).ready(function () {
         return arr[i];
     }
 
-    console.log(randomChoice(sectors));
+    //function to 'play' the choices from an array
+    function playChoices(arr) {
+        arr.forEach(function (el, i) {
+            el.delay(1000 * i).animate({opacity: 1}, 1000, function () {
+                el.animate({opacity: 0.6});
+            });
+        });
+    }
+
+    playChoices(sectors);
+
 });
