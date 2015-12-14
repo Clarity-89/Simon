e1441f69249db79e575abfd4b415edaea0edfb47
Alex <Clarity-89@users.noreply.github.com>
2015-12-14 11:58:15 +0200

    Fix duplicate audio file link

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
         blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
         red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-        yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+        yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
         error: new Audio('audio/error.wav'),
         win: new Audio('audio/win.mp3')
     };
